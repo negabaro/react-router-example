873ceaf081ea2a2e00857458345d3dcd07b168a1
negabaro kim <negabaro@naver.com>
2018-12-01 06:49:01 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## react-router-4-example
+
+https://github.com/eh3rrera/react-router-4-example
---
+++ react-router-4-example-clone/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    devServer: {
+      host: '0.0.0.0',
+      disableHostCheck: true
+    },
+    resolve: {
+      extensions: ['.jsx', '.js', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js|\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+           test: /\.css$/,
+           use: ['style-loader', 'css-loader'],
+        }
+      ]
+    }
+  };
+  
+  
+  
